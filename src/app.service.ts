2e69d7e3caf49fd02d9d2406760d1e8244ffa7e3
Chandan Vishwakarma <sanyukt1994@gmail.com>
2024-05-21 17:16:15 +0530

    chore: enable lambda insights & wtfnode

--- src/app.service.ts
+++ src/app.service.ts
@@ -1,8 +1,12 @@
 import { Injectable } from '@nestjs/common';
 
+import wtf from 'wtfnode';
+
 @Injectable()
 export class AppService {
   getHello(): string {
+    wtf.dump();
+
     return 'Hello World!';
   }
 }
